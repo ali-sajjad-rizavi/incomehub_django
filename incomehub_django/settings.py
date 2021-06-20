717af33cbefba1a284cd8e56907b48e4dd6c3ede
Ali <sasrizavi@gmail.com>
2021-06-20 19:26:31 +0500

    Comment added

--- incomehub_django/settings.py
+++ incomehub_django/settings.py
@@ -164,4 +164,4 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals(), databases=False)
+django_heroku.settings(locals(), databases=False)  # For heroku
